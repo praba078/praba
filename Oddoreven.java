ea23aa0e6edf37874b6c6acc4e4a14bdc3179522
praba078 <prabashalini75@gmail.com>
2017-06-19 14:50:35 +0530

    Add files via upload

---
+++ Oddoreven.java
@@ -0,0 +1,17 @@
+package Guvi;
+import java.util.*;
+public class Oddoreven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the number");
+		int a=s.nextInt();
+		if(a%2==0)
+			System.out.println("Number is even");
+		else
+			System.out.println("Number is odd");}
+
+	}
+
+
