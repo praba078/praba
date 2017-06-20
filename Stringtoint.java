d68b8212d7b9ef5c93f946ef7cc9b877a3cd4764
praba078 <prabashalini75@gmail.com>
2017-06-20 09:55:43 +0530

    Add files via upload

---
+++ Stringtoint.java
@@ -0,0 +1,16 @@
+package Guvi;
+import java.util.*;
+public class Stringtoint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s;
+		int n;
+Scanner in=new Scanner(System.in);
+System.out.println("enter");
+s=in.nextLine();
+n=Integer.parseInt(s);
+System.out.println(n);
+	}
+
+}
