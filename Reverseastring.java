4144c35f25e8b89a8d40b0df6a2e33b772037f02
praba078 <prabashalini75@gmail.com>
2017-06-20 09:18:11 +0530

    Add files via upload

---
+++ Reverseastring.java
@@ -0,0 +1,14 @@
+package Guvi;
+import java.util.*;
+public class Reverseastring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner u=new Scanner(System.in);
+System.out.println("enter");
+String ori=u.next();
+String rev=new StringBuffer(ori).reverse().toString();
+System.out.println(rev);
+	}
+
+}
