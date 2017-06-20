ae34edaec4fa289ef6a90b55e49d644d26be6951
praba078 <prabashalini75@gmail.com>
2017-06-20 11:11:35 +0530

    Add files via upload

---
+++ Minnum.java
@@ -0,0 +1,17 @@
+package Guvi;
+import java.util.*;
+public class Minnum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner s=new Scanner(System.in);
+System.out.println("enter");
+int a=s.nextInt();
+int b=s.nextInt();
+int c,d,sp;
+c=a%10;
+d=b%10;
+sp=c-d;
+	}
+
+}
