5868b899ce070264e993302499b7bdec6470cb0b
praba078 <prabashalini75@gmail.com>
2017-07-06 11:13:02 +0530

    Add files via upload

---
+++ Power_num.java
@@ -0,0 +1,15 @@
+package Guvi;
+
+import java.util.Scanner;
+
+public class Power_num {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sp=new Scanner(System.in);
+		int num=sp.nextInt();
+		int power=sp.nextInt();
+		System.out.println(Math.pow(num,power));
+	}
+
+}
