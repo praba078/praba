45585f90586ffb624293c0cf52ded5087281df8d
praba078 <prabashalini75@gmail.com>
2017-08-10 15:04:02 +0530

    Add files via upload

--- Palindrome.java
+++ Palindrome.java
@@ -1,4 +1,4 @@
-package String;
+package Guvi;
 
 import java.util.Scanner;
 
@@ -6,19 +6,28 @@ public class Palindrome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner u=new Scanner(System.in);
-		System.out.println("enter");
-		String ori=u.next();
-		String rev=new StringBuffer(ori).reverse().toString();
-		System.out.println(rev);
-		if(ori.equals(rev))
-		{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int r=0;
+		int s=0;
+		int ri=0;
+		int rev=0;
+		while(a!=0){
+			r=a%10;
+			s=s+r;
+			a=a/10;
+		}
+		while(s!=0){
+			ri=s%10;
+			rev=rev*10+ri;
+			s=s/10;
+		}
+		if(s==rev){
 			System.out.println("palindrome");
 		}
-		else
-		{
-			System.out.println("not a palindrome+");
-		}
+		else{
+			System.out.println("not palindrome");
+}
 	}
 
 }
