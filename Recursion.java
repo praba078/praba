594270108d074d9910e46e47c5838505653c4147
praba078 <prabashalini75@gmail.com>
2017-08-12 11:51:20 +0530

    Add files via upload

---
+++ Recursion.java
@@ -0,0 +1,19 @@
+package Guvi;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+public class Recursion {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner sp=new Scanner(System.in);
+		 ArrayList<Integer>sd=new  ArrayList<Integer>();
+		 sd.add(1);
+		 sd.add(2);
+		 sd.add(3);
+		 sd.add(4);
+		 sd.add(5);
+		 Collections.reverse(sd);
+		System.out.println("the reverse is"+sd); 
+	}
+
+}
