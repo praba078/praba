083fdef7b48397919e052f4daff217b6948db908
praba078 <prabashalini75@gmail.com>
2017-09-30 10:23:57 +0530

    Add files via upload

---
+++ StrlenMethod.java
@@ -0,0 +1,15 @@
+package Praba;
+import java.util.Scanner;
+public class StrlenMethod {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sp=new Scanner(System.in);
+		String s=sp.nextLine();
+		String s1=sp.nextLine();
+		int a=s.length();
+		int b=s1.length();
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
