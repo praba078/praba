4b38d6cd019b654658e2f5857f8a708cd22cdfe4
praba078 <prabashalini75@gmail.com>
2017-09-30 10:38:18 +0530

    Add files via upload

---
+++ ContainsMethod.java
@@ -0,0 +1,17 @@
+package Praba;
+
+import java.util.Scanner;
+
+public class ContainsMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sp=new Scanner(System.in);
+		String s="I am praba and studying final year ECE";
+		System.out.println(s.contains("am"));
+        System.out.println(s.contains("&"));
+        System.out.println(s.contains("praba"));
+        System.out.println(s.contains("civil"));
+	}
+
+}
